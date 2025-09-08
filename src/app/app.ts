534eb1953d04d0536f6432e7f7602bf8846f3e44
Amrittpal Kahlon <w0877926@myscc.ca>
2025-09-08 14:48:19 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,18 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Amritpal-Singh-learning-angular');
+  //Lets make a variable
+  name : string = "Amrit"
+  age : string = "20"
+  studysat : string = "StClair College"
+
+  //Once you assign a var type, it is forever that type
+  //name = 7 <
 }
+
