779a7ea09fdf6ba9fba7935ef9d316c19462da05
Amritpal Singh <amritpalsingh10049@gmail.com>
2025-09-26 12:05:52 -0400

    display muscle car list, made the css page and html page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MuscleCar } from './models/muscle-car';
+import {NgFor, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [NgFor, NgIf],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('Amritpal-Singh-learning-angular');
-  //Let's make a variable
-  name : string = "Amrit"
-  age : string = "20"
-  studysat : string = "StClair College"
+  title: string = 'Muscle Car Showcase';
 
-  //Once you assign a var type, it is forever that type
-  //name = 7 <
+  muscleCars: MuscleCar[] = [
+    { make: 'Ford', model: 'Mustang Boss 429', year: 1969, horsepower: 375, topSpeed: 118, isClassic: true },
+    { make: 'Chevrolet', model: 'Camaro Z/28', year: 1969, horsepower: 290, topSpeed: 120, isClassic: true },
+    { make: 'Dodge', model: 'Charger R/T', year: 1970, horsepower: 375, topSpeed: 127, isClassic: true },
+    { make: 'Plymouth', model: 'Barracuda HEMI', year: 1970, horsepower: 425, topSpeed: 117, isClassic: true },
+    { make: 'Pontiac', model: 'GTO Judge', year: 1970, horsepower: 370, topSpeed: 115, isClassic: true },
+    { make: 'Chevrolet', model: 'Chevelle SS 454', year: 1970, horsepower: 450, topSpeed: 125, isClassic: true },
+    { make: 'Dodge', model: 'Challenger R/T', year: 1970, horsepower: 375, topSpeed: 124, isClassic: true },
+    { make: 'AMC', model: 'AMX 390', year: 1969, horsepower: 315, topSpeed: 115 },
+    { make: 'Buick', model: 'GSX Stage 1', year: 1970, horsepower: 360, topSpeed: 123 },
+    { make: 'Oldsmobile', model: '442 W-30', year: 1970, horsepower: 370, topSpeed: 121 },
+  ];
 }
-
