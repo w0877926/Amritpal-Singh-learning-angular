eeb017f3a38a62b765533cf99ba6937529284445
Amritpal Singh <amritpalsingh10049@gmail.com>
2025-09-30 01:45:44 -0400

    generated car-list and car-list-item

---
+++ src/app/car-list-item/car-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-list-item',
+  imports: [],
+  templateUrl: './car-list-item.component.html',
+  styleUrl: './car-list-item.component.css'
+})
+export class CarListItemComponent {
+
+}
