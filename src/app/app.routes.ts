8d0d557ccc35d7ce41be4fa22012394cae81ce9a
Amritpal Singh <amritpalsingh10049@gmail.com>
2025-10-14 11:23:27 -0400

    defined routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CarListComponent } from './car-list/car-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CarListComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' }
+];
