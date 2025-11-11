2d3c07ad0636e887131a03a395761485cb20aef2
Amritpal Singh <amritpalsingh10049@gmail.com>
2025-11-11 11:44:18 -0500

    added lazy loading final commit

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,44 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-import { CarListComponent } from './app/car-list/car-list.component';
-import { CarListItemComponent } from './app/car-list-item/car-list-item.component';
-import { ModifyListItemComponent } from './app/modify-list-item/modify-list-item.component';
-import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './app/services/in-memory-data.service';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 
+import { App } from './app/app';
+import { CarListComponent } from './app/car-list/car-list.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './app/services/in-memory-data.service';
+
 const routes: Routes = [
   { path: '', redirectTo: '/cars', pathMatch: 'full' },
+
   { path: 'cars', component: CarListComponent },
-  { path: 'cars/:id', component: CarListItemComponent },
-  { path: 'modify', component: ModifyListItemComponent },
-  { path: 'modify/:id', component: ModifyListItemComponent},
-  { path: '**', component: PageNotFoundComponent }
+
+  {
+    path: 'cars/:id',
+    loadComponent: () =>
+      import('./app/car-list-item/car-list-item.component')
+        .then(m => m.CarListItemComponent)
+  },
+
+  {
+    path: 'modify',
+    loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component')
+        .then(m => m.ModifyListItemComponent)
+  },
+  {
+    path: 'modify/:id',
+    loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component')
+        .then(m => m.ModifyListItemComponent)
+  },
+
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component')
+        .then(m => m.PageNotFoundComponent)
+  }
 ];
 
 bootstrapApplication(App, {
@@ -27,5 +48,5 @@ bootstrapApplication(App, {
     importProvidersFrom(
       HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 })
     )
-  ],
-}).catch((err) => console.error(err));
+  ]
+}).catch(err => console.error(err));
